04bcc40d4d08afdc3467141cd81ef80e9ab7bade
NuwanSD <sagaradnuwan@gmail.com>
2024-06-07 18:37:13 +0530

    added new features

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  // document IDs
+  List<String> docIDs = [];
+
+  Future getDocId() async {
+    await FirebaseFirestore.instance.collection('data').get().then(
+          (snapshot) => snapshot.docs.forEach((document) {
+            print(document.reference);
+            docIDs.add(document.reference.id);
+          }),
+        );
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getData();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +38,32 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('Signed in as : ' + user.email!),
+          SizedBox(
+            height: 10,
+          ),
           MaterialButton(
             onPressed: () {
               FirebaseAuth.instance.signOut();
             },
             color: Colors.deepPurple[200],
             child: Text("Sign out"),
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+              child: FutureBuilder(
+                  future: getDocId(),
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      itemCount: docIDs.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(docIDs[index]),
+                        );
+                      },
+                    );
+                  })),
         ],
       )),
     );
